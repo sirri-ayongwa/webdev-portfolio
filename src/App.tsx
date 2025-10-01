1359e4d0bbbfcec43cad16294af6dc965c134e67
Sirri Ayongwa <sirribongnwi7@gmail.com>
2025-10-02 02:23:32 +0600

    Replace inject with SpeedInsights React component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { inject } from '@vercel/speed-insights';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
@@ -34,13 +34,11 @@ const App = () => (
           </main>
           <Footer />
           <BackToTop />
+          <SpeedInsights />
         </div>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
-
-// Inject Vercel Speed Insights
-inject();
 
 export default App;
