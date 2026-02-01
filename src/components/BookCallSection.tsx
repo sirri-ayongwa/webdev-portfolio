a3bb40310d55ddebb015adb693b0bddf5616b9e9
Sirri <105353083+sirri-ayongwa@users.noreply.github.com>
2026-02-01 08:05:36 +0600

    Update section tag with an id attribute

--- src/components/BookCallSection.tsx
+++ src/components/BookCallSection.tsx
@@ -14,7 +14,7 @@ const BookCallSection = () => {
   }, []);
 
   return (
-    <section className="py-20 px-4 sm:px-6 lg:px-8">
+    <section id="contact" className="py-20 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto max-w-4xl">
         <ScrollReveal>
           <div className="text-center">
